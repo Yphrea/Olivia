8d7ac0d006c56f4b7a6d01e5ebf0e87ae02c425d
Eyching <pernilla.helmer@protonmail.com>
2026-04-10 09:20:54 +0200

    Add all tomes, some thumbnails and some layout tweaks

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ myCollection = spellCollection.from_yaml("Olivia_spells.yaml")
 #    print(item.to_tikz())
 
 OliviaTex = latexDocument('Olivia.tex')
+
+#Tomes section
 OliviaTex.addContent("""\\section*{Tomes}\n
 \\scriptsize\n""")
 tomeMulticol = latexEnvironment('multicols*', required='3')
@@ -17,8 +19,21 @@ for i, item in enumerate(myCollection.tomes):
     tomeMulticol.addContent(tikz, setParent=True)
 
 OliviaTex.addContent("\\normalfont\n")
-#print(OliviaTex)
-#OliviaTex.write()
+
+#Spellpages section
+OliviaTex.addContent("""\\section*{spellpages}\n
+\\scriptsize\n""")
+tomeMulticol = latexEnvironment('multicols*', required='3')
+OliviaTex.addContent(tomeMulticol, setParent=True)
+for i, item in enumerate(myCollection.spellpages):
+    tikz = latexEnvironment('tikzpicture')
+    tikz.addContent(item.toTikz())
+    tomeMulticol.addContent(tikz, setParent=True)
+
+OliviaTex.addContent("\\normalfont\n")
+
+
+#compilation
 OliviaTex.compile()
 
 
